9e4471444346a019727c93096d7060d8860f2e7b
Pe13 <paolo.peforni@gmail.com>
2024-08-04 18:29:00 +0200

    Configured the scene creation (maybe)

--- tcAppDelegate.mm
+++ tcAppDelegate.mm
@@ -1,12 +1,28 @@
 #import "tcAppDelegate.h"
+#include <Foundation/NSObjCRuntime.h>
+#include <UIKit/UIWindowScene.h>
+#include <UIKit/UISceneSession.h>
 #import "tcSceneDelegate.h"
 
 @implementation tcAppDelegate
 
-- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
-	_mainScene = [[tcSceneDelegate alloc] init];
+- (BOOL)application:(UIApplication *)application
+    didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  _mainScene = [[tcSceneDelegate alloc] init];
 
-	return YES;
+  return YES;
+}
+
+- (UISceneConfiguration *)application:(UIApplication *)application
+    configurationForConnectingSceneSession:(UISceneSession *)connectingSceneSession
+    options:(UISceneConnectionOptions *)options {
+
+  UISceneConfiguration * out = [[UISceneConfiguration alloc] init];
+
+  out.sceneClass = NSClassFromString(@"UIWindowScene");
+  out.delegateClass = NSClassFromString(@"tcSceneDelegate");
+
+  return out;
 }
 
 @end
