01190ceef73de5fdea1312931ef3cc973bf6d9ca
Pe13 <paolo.peforni@gmail.com>
2024-08-05 16:08:21 +0200

    Adopt the right protocol

--- include/tcSceneDelegate.h
+++ include/tcSceneDelegate.h
@@ -1,6 +1,6 @@
 #import <UIKit/UIKit.h>
 
-@interface tcSceneDelegate: UIResponder <UISceneDelegate>
+@interface tcSceneDelegate: UIResponder <UIWindowSceneDelegate>
 
 @property (nonatomic, strong) UIWindow *window;
 @property (nonatomic, strong) UINavigationController *rootViewController;
